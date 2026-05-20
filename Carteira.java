32f895a9396ea266d6225b46f5c80feaf1c29dca
João <joaosantanaguilherme@gmail.com>
2026-05-19 21:14:27 -0300

    feat_seguranca

--- Carteira.java
+++ Carteira.java
@@ -2,10 +2,12 @@ public class Carteira{
 
     String dono;
     double saldoBitcoin;
+    String senhaAcesso;
 
-    public Carteira(String dono){
+    public Carteira(String dono, String senhaAcesso){
         this.dono = dono;
         this.saldoBitcoin = 0.001;
+        this.senhaAcesso = senhaAcesso;
     }
 
     public void depositar(double qtd){
--- Principal.java
+++ Principal.java
@@ -1,7 +1,7 @@
 public class Principal {
     
     public static void main(String[] args){
-        Carteira minhaCarteira = new Carteira("Thiago");
+        Carteira minhaCarteira = new Carteira("Thiago", "1234");
         minhaCarteira.depositar(0.5);
         System.out.println("Saldo Bitcoin: "+minhaCarteira.saldoBitcoin);
     }
